5146707eba14a3a010f9f0df17eb15953d901e21
e.rose <e.rose@student.isartdigital.com>
2025-10-03 10:48:32 +0200

    [WIP] rework wip

--- PhysicsEngine/Assets/Scripts/PhysicsManager.cs
+++ PhysicsEngine/Assets/Scripts/PhysicsManager.cs
@@ -44,12 +44,17 @@ public class PhysicsManager : MonoBehaviour
         
     }
     
-    List<AABB> bounds = new List<AABB>();
-    List<CustomCollider> colliders = new List<CustomCollider>();
+    // Existing collider in the scene
+    private List<CustomCollider> colliders = new List<CustomCollider>();
+    // Existing bounds
+    private List<AABB> collidersBounds = new List<AABB>();
     
-    List<Node> boundsTree = new List<Node>();
-    int root;
-    // {index, aabb, collider} : {0, aabb1, null}
+    // All bounds inside tree, include both existing collider and abstract detection zone
+    private List<AABB> bounds = new List<AABB>();
+    // AABB tree
+    private List<Node> boundsTree = new List<Node>();
+    // Index of the root 
+    private int root;
     
     void Start()
     {
@@ -59,9 +64,10 @@ public class PhysicsManager : MonoBehaviour
         {
             collider.InitAABB();
             AABB bound = collider.GetAABB();
+            
+            collidersBounds.Add(bound);
             bounds.Add(bound);
         }
-        
         BuildAABBTree();
     }
     
@@ -87,14 +93,14 @@ public class PhysicsManager : MonoBehaviour
 
     public void BuildAABBTree()
     {
-        if (bounds.Count == 0)
+        if (collidersBounds.Count == 0)
         {
             Debug.Log("No colliders found in the scene");
             return;
         }
 
-        // Special case if there is only one collider
-        if (bounds.Count == 1)
+        // Special case if there is only one collider, it become the root
+        if (collidersBounds.Count == 1)
         {
             Node first = new Node(-1, -1, -1, true, 0); // 0 beceause there is only one collider so only one aabb in list
             boundsTree.Add(first);
@@ -102,7 +108,7 @@ public class PhysicsManager : MonoBehaviour
             return;
         }
 
-        foreach (AABB bound in bounds) // issue of logic here, 
+        foreach (AABB bound in collidersBounds)
         {
             InsertAABB(bound);
         }
@@ -110,62 +116,72 @@ public class PhysicsManager : MonoBehaviour
     
     public void InsertAABB(AABB bound)
     {
+        // If the tree is empty the first bound become the root
         if (boundsTree.Count == 0)
         {
             Node node = new Node(-1, -1, -1, true, 0);
             boundsTree.Add(node);            
         }
 
+        // If it's an existing collider or a abstract detection zone
         bool isLeaf = false;
         Node currentNode = null;
 
-        int newIndex = 0;
-        bool isRight = true;
-        while (!isLeaf)
+        while (!currentNode.isLeaf)
         {
-            if (currentNode.leftIndex == -1 && currentNode.rightIndex == -1)
-            {
-                isLeaf = true;
-                continue;
-            }
             float leftValue = AABB.GetUnionCost(bounds[boundsTree[currentNode.leftIndex].AABBIndex], bound);
             float rightValue = AABB.GetUnionCost(bounds[boundsTree[currentNode.rightIndex].AABBIndex], bound);
-
-            if (leftValue < rightValue)
-            {
-                newIndex = currentNode.leftIndex;
-                currentNode = boundsTree[currentNode.leftIndex];
-                isRight = false;
-            }
-            else
-            {
-                newIndex = currentNode.rightIndex;
-                currentNode = boundsTree[currentNode.rightIndex];
-            }
+            
+            
         }
-
-        AABB newParentAABB = new AABB();
-        newParentAABB.SetAABB(bounds[currentNode.AABBIndex], bound);
-        bounds.Add(newParentAABB);
-        
-        Node newParentNode = null;
-        
-        currentNode.parentIndex = boundsTree.Count;
-
-        bounds.Add(bound);
-        Node newBoundsNode = new Node(boundsTree.Count, -1, -1, true, bounds.Count - 1);
-        boundsTree.Add(newBoundsNode);
-        
-        if (isRight)
-        {
-            // issue with left (change -1)
-            newParentNode = new Node(currentNode.parentIndex, boundsTree.Count, newIndex,false, bounds.Count - 1);
-        }
-        else
-        {
-            // issue with right (change -1)
-            newParentNode = new Node(currentNode.parentIndex, newIndex, boundsTree.Count,false, bounds.Count - 1);
-        }
-        boundsTree.Add(newParentNode);
+        //int newIndex = 0;
+        //bool isRight = true;
+        //
+        //while (!isLeaf)
+        //{
+        //    if (currentNode.leftIndex == -1 && currentNode.rightIndex == -1)
+        //    {
+        //        isLeaf = true;
+        //        continue;
+        //    }
+        //    float leftValue = AABB.GetUnionCost(bounds[boundsTree[currentNode.leftIndex].AABBIndex], bound);
+        //    float rightValue = AABB.GetUnionCost(bounds[boundsTree[currentNode.rightIndex].AABBIndex], bound);
+        //
+        //    if (leftValue < rightValue)
+        //    {
+        //        newIndex = currentNode.leftIndex;
+        //        currentNode = boundsTree[currentNode.leftIndex];
+        //        isRight = false;
+        //    }
+        //    else
+        //    {
+        //        newIndex = currentNode.rightIndex;
+        //        currentNode = boundsTree[currentNode.rightIndex];
+        //    }
+        //}
+        //
+        //AABB newParentAABB = new AABB();
+        //newParentAABB.SetAABB(bounds[currentNode.AABBIndex], bound);
+        //bounds.Add(newParentAABB);
+        //
+        //Node newParentNode = null;
+        //
+        //currentNode.parentIndex = boundsTree.Count;
+        //
+        //bounds.Add(bound);
+        //Node newBoundsNode = new Node(boundsTree.Count, -1, -1, true, bounds.Count - 1);
+        //boundsTree.Add(newBoundsNode);
+        //
+        //if (isRight)
+        //{
+        //    // issue with left (change -1)
+        //    newParentNode = new Node(currentNode.parentIndex, boundsTree.Count, newIndex,false, bounds.Count - 1);
+        //}
+        //else
+        //{
+        //    // issue with right (change -1)
+        //    newParentNode = new Node(currentNode.parentIndex, newIndex, boundsTree.Count,false, bounds.Count - 1);
+        //}
+        //boundsTree.Add(newParentNode);
     }
 }
